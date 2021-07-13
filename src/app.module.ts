b595a19482aa18c4135185630842428466f9d534
Niels Schmitt <niels.schmitt@sap.com>
2021-07-13 16:43:48 +0200

    enable for compare with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,17 +11,36 @@ import { Photo } from './photo.entity';
     TypeOrmModule.forFeature([Photo]),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'sap',
-        host: configService.get('HANA_HOST'),
-        port: 443,
-        username: configService.get('HANA_USER'),
-        password: configService.get('HANA_PASSWORD'),
-        entities: [Photo],
-        synchronize: true,
-        encrypt: true,
-        sslValidateCertificate: false,
-      }),
+      useFactory: (configService: ConfigService) => {
+        switch (configService.get('DB_TYPE')) {
+          case 'sap':
+            console.log('Connecting to HANA...');
+            return {
+              type: 'sap',
+              host: configService.get('HANA_HOST'),
+              port: configService.get('HANA_PORT'),
+              username: configService.get('HANA_USER'),
+              password: configService.get('HANA_PASSWORD'),
+              schema: configService.get('HANA_SCHEMA'),
+              entities: [Photo],
+              synchronize: true,
+              encrypt: true,
+              sslValidateCertificate: false,
+            };
+          case 'postgres':
+            console.log('Connecting to Postgres...');
+            return {
+              type: 'postgres',
+              host: configService.get('POSTGRES_HOST'),
+              port: configService.get('POSTGRES_PORT'),
+              username: configService.get('POSTGRES_USER'),
+              password: configService.get('POSTGRES_PASSWORD'),
+              schema: configService.get('POSTGRES_SCHEMA'),
+              entities: [Photo],
+              synchronize: true,
+            };
+        }
+      },
       inject: [ConfigService],
     }),
   ],
